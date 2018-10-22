6b2398a9c1a1ff43c6a8474e9bdb72c587202d20
Kenny Whyte <kawhyte@gmail.com>
2018-10-22 15:52:53 -0400

    Added configuration

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+const config =  require('config');
+const morgan = require ('morgan');
 const express = require ('express');
+const helmet = require ('helmet');
 const Joi = require ('joi');
 const logger = require('./logger')
 const app  = express();
 
-app.use(express.json());
+// Config
+console.log( 'Application name: ' + config.get('name'));
+console.log( 'Mailname: ' + config.get('mail.host'));
+console.log( 'Mail password: ' + config.get('production.mail.password'));
 
+if(app.get('env')=== 'development'){
+    app.use(morgan('tiny'));
+    console.log('Morgan middleware enabled...')
+}
+//console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+//console.log(`app.get: ${app.get('env')}`);
+
+
+app.use(express.json());
+app.use(helmet());
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'))
 
 app.use(logger);
 
---
+++ public/readme.txt
@@ -0,0 +1,2 @@
+This is regular text file for testing 
+Hello world!
